9b8e8e998e9fab695673f24dfa6a38c2856d74de
tim177 <timaraw18@gmail.com>
2025-03-21 10:37:16 +0530

    added cookie in backend

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -17,8 +18,17 @@ const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
+// Configure CORS to allow requests from your frontend
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true // Allow cookies or authorization headers
+};
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+//CORS
+app.use(cors(corsOptions));
 
 // 1) GLOBAL MIDDLEWARES
 // Serving static files
